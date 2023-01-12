da9c3c29861dddd448615f39d10f1eec7b8c667d
Leo <lechewu@gmail.com>
2023-01-12 14:51:02 +0800

    test(App): add tests

---
+++ src/__test__/App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { create } from 'react-test-renderer'
+
+import App from '../App'
+
+describe('App', () => {
+  test('should render well', () => {
+    expect(create(<App />).toJSON()).toMatchSnapshot()
+  })
+})
