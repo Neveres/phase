b695f4b56b5f627c79cb155b31f2fdce8d33252a
Leo <lechewu@gmail.com>
2023-01-12 14:39:45 +0800

    feat: setup webgl testing env

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,7 @@
 module.exports = {
   jest: (config) => {
     config.coverageReporters = ['text-summary', 'html']
+    config.setupFiles = ['jest-webgl-canvas-mock', ...config.setupFiles]
 
     return config
   },
